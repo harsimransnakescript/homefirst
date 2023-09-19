a1813a023b2000b1a18fdab38dfce8b2159720b4
Kamalpreet Kaur <kamalpreetkaur@snakescript.com>
2023-09-19 17:09:04 +0530

    Updated Otp Verification page and Created Products page

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth.decorators import login_required
+import csv
+
+from django.http import HttpResponse
 
 
 # Create your views here.
 @login_required
 def home(request):
-    products=ProductsModel.objects.all()
-    return render(request,"main_templates/home.html",{"products":products})
+    products = ProductsModel.objects.all()
+    return render(request, "main_templates/home.html", {"products": products})
+
+
+def import_csv_to_model(csv_file_path):
+    csv_file_path = "/Users/apple/Downloads/Homefirst DME Catalog.csv"
+
+    with open(csv_file_path, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            product = ProductsModel(
+                group=row["CATEGORY"],
+                name=row["Product Name"],
+                image=None,
+                specifications=row["DESCRIPTION"],
+                other_info=row["PRODUCT FEATURES"],
+                price=None,
+            )
+
+            product.save()
+        return HttpResponse("Categories Added")
+
+
+@login_required
+def products(request):
+    products = ProductsModel.objects.all()
+    return render(request, "main_templates/products.html", {"products": products})
