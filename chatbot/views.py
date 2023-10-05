49a020253451c9afcf2920c9b6bfbeb2a93a3edb
Kamalpreet Kaur <kamalpreetkaur@snakescript.com>
2023-10-05 18:31:48 +0530

    Updated slider for products

--- chatbot/views.py
+++ chatbot/views.py
@@ -12,16 +12,15 @@ from django.core import serializers
 openai.api_key = settings.OPENAI_API_KEY 
 
 def index(request):
+    
     return render(request,'templates/index3.html')
 
 class StreamGeneratorView(View):
-
-    def write(self,request,rawjsonData):
+    
+    def write(self,request,rawjsonData):    
         parsedJsonData = json.loads(rawjsonData.lstrip('$$').strip())
-        print(parsedJsonData)
         keywords_array = parsedJsonData.get('keywordsArray', [])
-        print(keywords_array)
-        
+  
         q_objects = Q()
         for value in keywords_array:
             q_objects |= Q(name__icontains=value) | Q(group__icontains=value)| Q(specifications__icontains=value) | Q(other_info__icontains=value)
@@ -32,6 +31,7 @@ class StreamGeneratorView(View):
             products_data = []
             for product in found_products:
                 product_info = {
+                    "id":product.id,
                     "name": product.name,
                     "image":str(product.image.url) if product.image else '',
                     "group": product.group,
@@ -49,22 +49,31 @@ class StreamGeneratorView(View):
                 "message": "No products found",
             }
    
-        print("---",response_data)   
         return response_data
 
         # return parsedJsonData['keywordsArray']
-        
-    def gpt3_5(self,request, prompt):
+     
+    def gpt3_5(self,request, prompt,selected_product):
         try:
             Instruction = {"role": "system", "content": """
     !IMPORTANT :1. You are a patient care expert website named Homefirst DME!, Your replies and suggestions are short and friendly, You search for all types of homecare medical products by replying in the mentioned way only\n
-                2. Always ask and Suggest for medical products like underpands, juvinile care, After the product main feature always ask for additional features like skin protection, fast absorption, long lasting etc \n
+                2. Always ask and Suggest for medical products like underpands,liner/pad,brief,personal care.After the product main feature always ask for additional features like skin protection, fast absorption, long lasting etc \n
                 3. When user asks to find or search for medical products Reply $$ { "keywordsArray":[ "feature1","feature1abbrevation","feature1synonym","feature2","feature2abbrevation","feature2synonym"]} , features should be of one word only, no camel case allowed, feature can not have two or more words or "-" inside it\n
                 4. Convert and include the products feature with multiple features with synonyms, short forms, variations and abbreviations as many as possible repeated and send inside the keywordArray\n
                 5. Do not mention i need keywords to search instead ask for features or other requirements in products, When asked do not proceed to search until you have any one of the features to search for the products, Do not mention i am unable and this information is provided from this website only
                   
             """}
-            conversation = [Instruction]+prompt
+            Instruction2 = {"role":"system","content":"""
+            !IMPORTANT :1. product is selected ask now patient information like first name.
+                        2. Ask about home delivery or office delivery. 
+                        3. Ask shipping address of product.
+                           """}
+           
+
+            if selected_product:
+                conversation = [Instruction2]+prompt
+            else:
+                conversation = [Instruction]+prompt
 
             response = openai.ChatCompletion.create(
                 model="gpt-4",  # Use the "gpt-3.5-turbo" model
@@ -97,61 +106,24 @@ class StreamGeneratorView(View):
             result = str(e)
             return JsonResponse({"result": result})
         
-    def case_manager_chat(self, request, prompt):
-        try:
-            instructions = {
-                "role": "system",
-                "content": """
-                    !IMPORTANT: You are a case manager assistant. Your goal is to assist with managing sample products for patient.
-                    1. Respond to user queries and requests related to patients.
-                    2. Ask for case details, such as title, description, or status, when necessary.
-                    3. Provide updates on case statuses and progress.
-                    4. Use keywords to search for specific cases. Reply with $$ { "keywordsArray": ["keyword1", "keyword2"] }
-                    5. Be concise and professional in your responses.
-                """
-            }
-            
-            conversation = [instructions] + prompt
-
-            response = openai.ChatCompletion.create(
-                model="gpt-4",  # Use the appropriate OpenAI model
-                messages=conversation,
-                temperature=0.5,
-                max_tokens=2048,
-                stream=True
-            )
-
-            json_data_string = ""
-            write_json_config = False
-
-            for chunk in response:
-                chunk_message = chunk['choices'][0]['delta']
-                word = chunk_message.get("content", '')
-                if "$$" in word:
-                    write_json_config = True
-
-                if write_json_config:
-                    json_data_string += word
-                    if "}" in word:
-                        write_json_config = False
-                        data = self.write(request, json_data_string)
-                        yield data
-                else:
-                    yield word
-
-        except Exception as e:
-            print(e)
-            result = str(e)
-            return JsonResponse({"result": result})
-        
     def post(self,request):
-        data = json.loads(request.body.decode('utf-8'))
-        #get message from request
-        message =  data.get('messages', [])
-        name = self.gpt3_5(request, message)
-        #return Response({},status.HTTP_200_OK)
-        response =  StreamingHttpResponse(name,status=200, content_type='text/event-stream')
-        return response
+        selected_product = request.POST.get('product_id')
+
+        if selected_product:
+                name = self.gpt3_5(request, [], selected_product)
+                print("=====",name)
+                response = StreamingHttpResponse(name, status=200, content_type='text/event-stream')
+                return response
+    
+        else:
+ 
+            data = json.loads(request.body.decode('utf-8'))
+            #get message from request
+            message =  data.get('messages', [])
+            name = self.gpt3_5(request, message, None)
+            #return Response({},status.HTTP_200_OK)
+            response =  StreamingHttpResponse(name,status=200, content_type='text/event-stream')
+            return response
 
 
 
